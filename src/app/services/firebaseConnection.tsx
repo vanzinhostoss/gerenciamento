4c013fdd7a4bf1c49d191c433749bb7f1b38a600
vanzinhostoss <vanzinhosantos791@gmail.com>
2025-02-11 21:59:31 -0300

    Alterando arquivos do firebase

--- src/app/services/firebaseConnection.tsx
+++ src/app/services/firebaseConnection.tsx
@@ -1,25 +1,49 @@
 
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+// import { getFirestore, collection } from 'firebase/firestore';
+// import { getStorage } from 'firebase/storage';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDuVXOMi9nW4FPMIGQESk3jbqoZGhhWj0Y",
+//   authDomain: "gerenciamento-os.firebaseapp.com",
+//   projectId: "gerenciamento-os",
+//   storageBucket: "gerenciamento-os.firebasestorage.app",
+//   messagingSenderId: "1039390575941",
+//   appId: "1:1039390575941:web:c943972278dd9db7c1422c",
+//   measurementId: "G-JSMQPPHJ2J"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+// const storage = getStorage(app);
+
+// export {auth, db, storage, collection}
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, collection } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// Configuração do Firebase com variáveis de ambiente
 const firebaseConfig = {
-  apiKey: "AIzaSyDuVXOMi9nW4FPMIGQESk3jbqoZGhhWj0Y",
-  authDomain: "gerenciamento-os.firebaseapp.com",
-  projectId: "gerenciamento-os",
-  storageBucket: "gerenciamento-os.firebasestorage.app",
-  messagingSenderId: "1039390575941",
-  appId: "1:1039390575941:web:c943972278dd9db7c1422c",
-  measurementId: "G-JSMQPPHJ2J"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
+// Inicializando o Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export {auth, db, storage, collection}
-
+export { auth, db, storage, collection };
