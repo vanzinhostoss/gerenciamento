989b0155418d8f8a27ce3a0a20bd659dffbb4f59
vanzinhostoss <vanzinhosantos791@gmail.com>
2025-04-02 21:46:38 -0300

    Puxei a pagina de login para o PAGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,70 @@
 "use client"
-import Header from "@/components/Header";
+import { AuthContext } from "@/app/contexts/auth"
+import Link from "next/link"
+import { useContext, useState } from "react"
+import { toast } from "react-toastify"
 
+const Login = () => {
 
-const Page = () => {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
 
+    const authCtx = useContext(AuthContext)
 
-  return (
-    <div>
-    
-       <Header />
-      
-    </div>
-  )
+    const handleLogin = async () => {
+        if(email !== "" && senha !== ""){
+            await authCtx?.signin(email, senha)
+        }else{
+            toast.error('Ops, preencha os campos')
+        }
+    }
+
+    return(
+        <div className=" bg-[#e0e0e0] h-screen w-full md:flex items-center justify-center">
+          
+            <div className="w-full max-w-[800px] h-[500px] flex ">
+                <div className="hidden md:flex bg-[#4F67E8] p-6  flex-col gap-6 items-center justify-center rounded-tl-3xl">
+                    <h2 className="text-center text-4xl font-bold text-white">Login</h2>
+                    <p className="text-md text-gray-300 w-64 text-center">Digite Usúario e Senha para entrar no Sistema</p>
+                    <p className="text-2xl font-bold text-white">Gestao <span className="text-3xl text-yellow-500">365</span></p>
+
+                <div className="w-full h-2 bg-gradient-to-r from-lime-300 via-orange-300 via-pink-400 to-sky-300" />
+
+                </div>
+                <div className="p-6 flex flex-col h-screen md:h-auto items-center justify-center bg-white w-full">
+                    <div className="md:hidden absolute top-20">
+                        <p className="text-center text-gray-600 text-3xl font-bold ">Login</p>
+                        <p className="text-center text-gray-600 my-3">Digite Usúario e Senha para entrar no Sistema</p>
+                    </div>
+                    <div className="flex flex-col justify-center w-full h-auto">
+                    <label htmlFor="" className="text-gray-400 text-xs">EMAIL</label>
+                    <input type="text" placeholder="Digite o seu email"
+                    className="w-full border-b border-gray-300 outline-none text-gray-400 p-1 mb-10"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    />
+                    <label htmlFor="" className="text-gray-400 text-xs ">SENHA</label>
+                    <input type="password" placeholder="Digite a senha"
+                    className="w-full border-b border-gray-300 outline-none text-gray-400 p-1 mb-10"
+                    value={senha}
+                    onChange={e => setSenha(e.target.value)}
+                    />
+
+                    <button className="bg-[#5271FF] py-2 px-3 w-full text-white rounded-sm"
+                    onClick={handleLogin}
+                    >
+                    {authCtx?.loadingAuth ? "Carregando..." : 'Entrar'}
+                    </button>
+
+                    <Link href={"/register"}
+                    className="text-gray-500 text-sm my-3 text-center"
+                    >Ainda não possui uma conta? Crie ja a sua! </Link>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Page;
-
+export default Login
--- src/app/services/firebaseConnection.ts
+++ src/app/services/firebaseConnection.ts
@@ -1,54 +1,24 @@
 
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore, collection } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDuVXOMi9nW4FPMIGQESk3jbqoZGhhWj0Y",
-//   authDomain: "gerenciamento-os.firebaseapp.com",
-//   projectId: "gerenciamento-os",
-//   storageBucket: "gerenciamento-os.firebasestorage.app",
-//   messagingSenderId: "1039390575941",
-//   appId: "1:1039390575941:web:c943972278dd9db7c1422c",
-//   measurementId: "G-JSMQPPHJ2J"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-// const storage = getStorage(app);
-
-// export {auth, db, storage, collection}
-
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, collection } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDuVXOMi9nW4FPMIGQESk3jbqoZGhhWj0Y",
+  authDomain: "gerenciamento-os.firebaseapp.com",
+  projectId: "gerenciamento-os",
+  storageBucket: "gerenciamento-os.firebasestorage.app",
+  messagingSenderId: "1039390575941",
+  appId: "1:1039390575941:web:c943972278dd9db7c1422c",
+  measurementId: "G-JSMQPPHJ2J"
 };
 
 // Initialize Firebase
-let app;
-let auth;
-let db;
-let storage;
+const app = initializeApp(firebaseConfig);
 
-if (typeof window !== 'undefined' && !getApps().length) {
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  db = getFirestore(app);
-  storage = getStorage(app);
-}
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db, storage, collection };
+export {auth, db, storage, collection}
